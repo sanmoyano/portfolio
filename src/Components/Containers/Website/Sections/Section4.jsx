27cf56c8b0eed53aec6249a0a7ebd3a54ac0f85d
sanmoyano <smoyano.di@gmail.com>
2023-03-22 22:48:56 -0300

    Titl logo and title

--- src/Components/Containers/Website/Sections/Section4.jsx
+++ src/Components/Containers/Website/Sections/Section4.jsx
@@ -1,8 +1,12 @@
-import { Box, Text } from "@chakra-ui/react"
+import { Box, Text, useMediaQuery } from "@chakra-ui/react"
 import { Link } from "react-router-dom"
 import Tilt from "react-parallax-tilt"
 
 const Section4 = () => {
+  const isMobile = useMediaQuery("(max-width: 320px)")
+
+  console.log(isMobile[0])
+
   return (
     <Box
       height={{ base: "150px", md: "350px" }}
