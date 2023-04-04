462966af94b92f22a84372a7c51a532451842864
sanmoyano <smoyano.di@gmail.com>
2023-04-04 15:33:56 -0300

    Fixing tagCloud

--- src/Components/Containers/Website/Components/TextSphere.jsx
+++ src/Components/Containers/Website/Components/TextSphere.jsx
@@ -1,28 +1,18 @@
-import { Stack } from "@chakra-ui/react"
-import TagCloud from "TagCloud"
+import TagCloud from "@frank-mayer/react-tag-cloud"
 
 const TextSphere = ({ tags }) => {
-  const options = {
-    // radius in px
-    radius: 300,
-
-    // animation speed
-    // slow, normal, fast
-    maxSpeed: "fast",
-    initSpeed: "fast",
-
-    // 0 = top
-    // 90 = left
-    // 135 = right-bottom
-    direction: 135,
-
-    // interact with cursor move on mouse out
-    keep: true,
-  }
-
-  // TagCloud(".content", tags, options)
-
-  return <Stack className="content" />
+  return (
+    <TagCloud
+      options={(w) => ({
+        radius: Math.min(500, w.innerWidth, w.innerHeight) / 2,
+        maxSpeed: "fast",
+      })}
+      onClick={(tag, ev) => alert(tag)}
+      onClickOptions={{ passive: true }}
+    >
+      {tags}
+    </TagCloud>
+  )
 }
 
 export default TextSphere
