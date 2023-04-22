91ac991ca65d24bbc43c965dfa3dc20a5b92a0be
sanmoyano <smoyano.di@gmail.com>
2023-04-22 20:04:21 -0300

    skill card animation feature

--- src/Components/Containers/Website/Components/SkillCard.jsx
+++ src/Components/Containers/Website/Components/SkillCard.jsx
@@ -1,35 +1,38 @@
 import { Stack, Text, useColorModeValue } from "@chakra-ui/react"
 
-import { TagCloudText } from "."
+import { TagCloudText, ParallaxContainer } from "."
 
 const SkillCard = ({ type, list, isHovered, handleHover, index }) => {
   const color = useColorModeValue("red", "green")
 
   return (
-    <Stack
-      alignItems={"center"}
-      direction={"row"}
-      height={"100%"}
-      justifyContent={"space-between"}
-      marginTop={"0px !important"}
-      opacity={isHovered ? 1 : 0.2}
-      transition={"1.5s"}
-      onMouseEnter={() => handleHover(index)}
-      onMouseLeave={() => handleHover(0)}
-    >
-      <Text
-        color={color}
-        display={"inline-block"}
-        fontFamily={"Roboto"}
-        fontSize={"16rem"}
-        fontWeight={700}
-        lineHeight={1}
-        textTransform={"uppercase"}
+    <ParallaxContainer>
+      <Stack
+        alignItems={"center"}
+        direction={"row"}
+        height={"100%"}
+        id={index}
+        justifyContent={"space-between"}
+        marginTop={"0px !important"}
+        opacity={isHovered ? 1 : 0.2}
+        transition={"1.5s"}
+        onMouseEnter={() => handleHover(index)}
+        onMouseLeave={() => handleHover(0)}
       >
-        {type}
-      </Text>
-      <TagCloudText tags={list} />
-    </Stack>
+        <Text
+          color={color}
+          display={"inline-block"}
+          fontFamily={"Roboto"}
+          fontSize={"16rem"}
+          fontWeight={700}
+          lineHeight={1}
+          textTransform={"uppercase"}
+        >
+          {type}
+        </Text>
+        <TagCloudText tags={list} />
+      </Stack>
+    </ParallaxContainer>
   )
 }
 
