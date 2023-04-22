bb5dc897be9af23978a7558f57036365d6286610
sanmoyano <smoyano.di@gmail.com>
2023-04-22 20:31:12 -0300

    animate skill using stagger

--- src/Components/Containers/Website/Components/SkillCard.jsx
+++ src/Components/Containers/Website/Components/SkillCard.jsx
@@ -1,14 +1,16 @@
 import { Stack, Text, useColorModeValue } from "@chakra-ui/react"
+import { motion } from "framer-motion"
 
 import { TagCloudText, ParallaxContainer } from "."
 
-const SkillCard = ({ type, list, isHovered, handleHover, index }) => {
+const SkillCard = ({ type, list, isHovered, handleHover, index, animateSkill }) => {
   const color = useColorModeValue("red", "green")
 
   return (
     <ParallaxContainer>
       <Stack
         alignItems={"center"}
+        as={motion.div}
         direction={"row"}
         height={"100%"}
         id={index}
@@ -16,6 +18,7 @@ const SkillCard = ({ type, list, isHovered, handleHover, index }) => {
         marginTop={"0px !important"}
         opacity={isHovered ? 1 : 0.2}
         transition={"1.5s"}
+        variants={animateSkill}
         onMouseEnter={() => handleHover(index)}
         onMouseLeave={() => handleHover(0)}
       >
--- src/Components/Containers/Website/Components/SkillsBlock.jsx
+++ src/Components/Containers/Website/Components/SkillsBlock.jsx
@@ -8,13 +8,39 @@ import { SkillCard } from "./"
 
 const SkillsBlock = ({ dimension }) => {
   const [hoveredIndex, setHoveredIndex] = useState(0)
+  const containerSkills = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        duration: 0.1,
+        delayChildren: 0.5,
+        staggerChildren: 0.5,
+        staggerDirection: 1,
+      },
+    },
+  }
+
+  const itemSkill = {
+    hidden: { x: "-200%" },
+    show: { x: "0%" },
+  }
 
   return (
     <AnimatePresence>
-      <Stack height={dimension.height} justifyContent={"space-around"} width={"100%"}>
+      <Stack
+        as={motion.div}
+        height={dimension.height}
+        initial={"hidden"}
+        justifyContent={"space-around"}
+        variants={containerSkills}
+        whileInView={"show"}
+        width={"100%"}
+      >
         {skills.map(({ type, list }, index) => (
           <SkillCard
             key={type}
+            animateSkill={itemSkill}
             handleHover={setHoveredIndex}
             index={index}
             isHovered={hoveredIndex === index}
